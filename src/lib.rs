ec6cda860395f15d7a705b5895ecae956084553e
Steven Dwy <yoplitein+git@gmail.com>
2025-02-11 22:32:07 -0800

    Parse basic literals

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@
 use std::collections::HashMap;
 
 use nom::branch::alt;
-use nom::bytes::complete::{tag, take_while, take_while1};
-use nom::combinator::{eof, map, opt, recognize};
-use nom::multi::{many0_count, many1};
+use nom::bytes::complete::{is_not, tag, take_while, take_while1};
+use nom::combinator::{eof, map, map_res, not, opt, recognize, verify};
+use nom::error::{ErrorKind, FromExternalError};
+use nom::multi::{many0_count, many1, many1_count};
 use nom::sequence::terminated;
 use nom::{Finish, Parser};
+use nom_language::error::VerboseError;
 
 #[derive(Clone, Debug)]
 pub struct Grammar {
@@ -17,7 +19,7 @@ pub struct Grammar {
 
 #[derive(Clone, Debug)]
 pub enum Rule {
-	Literal(String),
+	Literal(Literal),
 	Rule(String),
 	Group(Vec<Rule>),
 	Alternate(Vec<Rule>),
@@ -28,6 +30,13 @@ pub enum Rule {
 	},
 }
 
+#[derive(Clone, Debug)]
+pub enum Literal {
+	Char(char),
+	String(String),
+	// TODO: HexChar/HexString for byte parsers
+}
+
 pub fn parse_grammar(input: &str) -> anyhow::Result<Grammar> {
 	let res = top.parse(input).finish();
 	let res = res.map_err(|err| anyhow::anyhow!("{err:#?}"));
@@ -35,7 +44,7 @@ pub fn parse_grammar(input: &str) -> anyhow::Result<Grammar> {
 	Ok(res)
 }
 
-type PResult<'a, T> = nom::IResult<&'a str, T, nom_language::error::VerboseError<&'a str>>;
+type PResult<'a, T> = nom::IResult<&'a str, T, VerboseError<&'a str>>;
 
 fn top(input: &str) -> PResult<Grammar> {
 	let (input, (rules, ..)) = (many1(rule), whitespace(true), eof).parse(input)?;
@@ -69,9 +78,102 @@ fn rule_end(input: &str) -> PResult<()> {
 }
 
 fn rule_body(input: &str) -> PResult<Rule> {
-	// TODO
-	let (input, rule_name) = identifier.parse(input)?;
-	Ok((input, Rule::Rule(rule_name)))
+	map(
+		many1((
+			alt((
+				map(identifier, Rule::Rule),
+				map(literal, Rule::Literal),
+			)),
+			alt((whitespace(false), rule_end)),
+		)),
+		|mut xs| {
+			if xs.len() == 1 {
+				xs.pop().unwrap_or_else(|| unreachable!()).0
+			} else {
+				Rule::Group(xs.into_iter().map(|v| v.0).collect())
+			}
+		},
+	)
+	.parse(input)
+}
+
+fn literal(input: &str) -> PResult<Literal> {
+	/* fn inner(char: bool) -> impl for<'a> Fn(&'a str) -> PResult<&'a str> {
+		let quote = if char {
+			"'"
+		} else {
+			"\""
+		};
+		move |input: &str| -> PResult<&str> {
+			let body = recognize(many1_count(alt((
+				dbg_dmp(map(recognize(not(tag(quote))), |v| dbg!(v)), "1"),
+				dbg_dmp(map(recognize((tag("\\"), tag(quote))), |v| dbg!(v)), "2"),
+			))));
+			map((
+				dbg_dmp(map(tag(quote), |v| dbg!(v)), "3"),
+				dbg_dmp(map(body, |v| dbg!(v)), "4"),
+				dbg_dmp(map(tag(quote), |v| dbg!(v)), "5"),
+			), |(_, v, _)| v).parse(input)
+		}
+	}
+	
+	alt((
+		map_res(inner(true), |v| {
+			let mut chars = v.chars();
+			let Some(char) = chars.next() else {
+				return Err(todo!("empty char literal"))
+			};
+			let None = chars.next() else {
+				// TODO: fold 
+				return Err(todo!("char literal with more than one character"))
+			};
+			Ok(Literal::Char(char))
+		}),
+		map(inner(false), |v| Literal::String(v.to_string())),
+	)).parse(input) */
+	
+	let input_start = input;
+	let (input, quote) = alt((
+		tag("'"),
+		tag("\""),
+	)).parse(input)?;
+	let Some(quote_char) = quote.chars().next() else { unreachable!() };
+	let (input, body) = recognize(many1_count(alt((
+		verify(
+			take_while(|c| c != quote_char && c != '\\'),
+			|str: &str| str.len() != 0,
+		),
+		recognize((tag("\\"), tag(quote))),
+	)))).parse(input)?;
+	let (input, _) = tag(quote).parse(input)?;
+	
+	let literal = match quote {
+		"'" => {
+			let mut chars = body.chars();
+			let Some(char) = chars.next() else {
+				return Err(nom::Err::Failure(VerboseError::from_external_error(
+					input_start,
+					ErrorKind::Alpha,
+					"empty char literal",
+				)))
+			};
+			let None = chars.next() else {
+				// TODO: replace escapes
+				return Err(nom::Err::Failure(VerboseError::from_external_error(
+					input_start,
+					ErrorKind::Alpha,
+					"char literal with more than one character",
+				)))
+			};
+			Literal::Char(char)
+		},
+		"\"" => {
+			Literal::String(body.to_string())
+		},
+		_ => unreachable!(),
+	};
+	dbg!(line!());
+	Ok((input, literal))
 }
 
 fn identifier(input: &str) -> PResult<String> {
@@ -112,4 +214,20 @@ fn whitespace(rule_start: bool) -> impl for<'a> Fn(&'a str) -> PResult<()> {
 		}
 		parser
 	}
+}
+
+pub fn dbg_dmp<'a, F, O, E: std::fmt::Debug>(
+  f: F,
+  context: &'static str,
+) -> impl Fn(&'a str) -> nom::IResult<&'a str, O, E>
+where
+  F: Fn(&'a str) -> nom::IResult<&'a str, O, E>,
+{
+  move |i: &'a str| match f(i) {
+    Err(e) => {
+      println!("{}: Error({:?}) at:\n{}", context, e, i);
+      Err(e)
+    }
+    a => a,
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@
 
 use nom::Parser;
 
+// #[cfg(none)]
 fn main() -> anyhow::Result<()> {
 	// top = 'a' 'b'
 	let grammar = r#"
-		top = wef
+		top = "ab"
 	"#;
 	let grammar = nbnf::parse_grammar(grammar)?;
 	dbg!(grammar);
 
+	Ok(())
+}
+
+#[cfg(none)]
+fn main() -> anyhow::Result<()> {
+	let inp = r#""ab""#;
+	dbg!(nbnf::literal(inp));
+	
 	Ok(())
 }
