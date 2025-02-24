3934fc2b68f8065ed8e386cfd451cb5fb62d6a63
Steven Dwy <yoplitein+git@gmail.com>
2025-02-24 15:34:10 -0800

    Pretty-print generated code

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use nom::Parser;
 fn main() -> anyhow::Result<()> {
 	// top = 'a' 'b'
 	let grammar = r#"
-		top = "hello";
+		top = "hello" "goodbye";
 	"#;
 	let grammar = nbnf::parse_grammar(grammar)?;
 	// let grammar = nbnf::lexer::lex(grammar)?;
 	dbg!(&grammar);
 	let parser = nbnf::generator::generate_parser(&grammar)?;
-	eprintln!("```\n{parser}\n```");
+	eprintln!("```{parser}```");
 
 	Ok(())
 }
