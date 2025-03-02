39047ba99ceca4c6d5d0405a771a73a6edcd6252
Steven Dwy <yoplitein+git@gmail.com>
2025-03-02 09:13:55 -0800

    Report macro errors as generated compile errors instead of panicking
    Plays nicer with rust-analyzer

--- nbnf_macro/src/lib.rs
+++ nbnf_macro/src/lib.rs
@@ -1,15 +1,21 @@
 #![allow(unused)]
 
+use quote::quote;
+
 #[proc_macro]
 pub fn nbnf(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	let str = tokens.to_string();
 	let grammar = match nbnf_language::parse_grammar(&str) {
 		Ok(g) => g,
-		Err(err) => panic!("couldn't parse given grammar: {err:#?}"),
+		Err(err) => return compile_error(&format!("couldn't parse given grammar: {err}")),
 	};
 	let tokens = match nbnf_language::generate_parser_tokens(&grammar) {
 		Ok(t) => t,
-		Err(err) => panic!("couldn't generate parser for given grammar: {err:#?}"),
+		Err(err) => return compile_error(&format!("couldn't generate parser for given grammar: {err}")),
 	};
 	tokens.into()
+}
+
+fn compile_error(msg: &str) -> proc_macro::TokenStream {
+	quote! { compile_error!(#msg); }.into()
 }
