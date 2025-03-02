6810581fa2c22086fc3f11571bc824eea715cc2a
Steven Dwy <yoplitein+git@gmail.com>
2025-03-02 12:47:29 -0800

    Require macro input to be passed as string
    Because macro invocations are still fed through Rust's lexer, valid grammar
    elements such as unbalanced quotes will cause it to choke

--- nbnf_macro/src/lib.rs
+++ nbnf_macro/src/lib.rs
@@ -6,30 +6,38 @@ use quote::quote;
 
 #[proc_macro]
 pub fn nbnf(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let str = tokens.to_string();
+	let str: syn::LitStr = match syn::parse(tokens) {
+		Ok(s) => s,
+		Err(err) => return compile_error(None, "grammar must be given as a string"),
+	};
+	let str = str.value();
 	let tokens = match nbnf_language::lexer::lex(&str) {
 		Ok(t) => t,
-		Err(err) => return compile_error(err, "couldn't lex given grammar"),
+		Err(err) => return compile_error(Some(err), "couldn't lex given grammar"),
 	};
 	let grammar = match nbnf_language::parser::parse(tokens) {
 		Ok(g) => g,
-		Err(err) => return compile_error(err, "couldn't parse given grammar"),
+		Err(err) => return compile_error(Some(err), "couldn't parse given grammar"),
 	};
 	let tokens = match nbnf_language::generate_parser_tokens(&grammar) {
 		Ok(t) => t,
-		Err(err) => return compile_error(err, "couldn't generate parser for given grammar"),
+		Err(err) => return compile_error(Some(err), "couldn't generate parser for given grammar"),
 	};
 	tokens.into()
 }
 
-fn compile_error(err: anyhow::Error, msg: &str) -> proc_macro::TokenStream {
-	let mut causes = String::new();
-	for cause in err.chain() {
-		if !causes.is_empty() {
-			causes.write_str("\ncaused by: ").unwrap();
+fn compile_error(err: Option<anyhow::Error>, msg: &str) -> proc_macro::TokenStream {
+	let msg = if let Some(err) = err {
+		let mut causes = String::new();
+		for cause in err.chain() {
+			if !causes.is_empty() {
+				causes.write_str("\ncaused by: ").unwrap();
+			}
+			causes.write_fmt(format_args!("{cause}")).unwrap();
 		}
-		causes.write_fmt(format_args!("{cause}")).unwrap();
-	}
-	let msg = format!("{msg}: {causes}");
+		&format!("{msg}: {causes}")
+	} else {
+		msg
+	};
 	quote! { compile_error!(#msg); }.into()
 }
