581535ae25558ee5696eb003b3c9d4395e664bf0
Steven Dwy <yoplitein+git@gmail.com>
2025-03-16 01:19:09 -0700

    Fix JSON example not handling negative numbers

--- examples/json.rs
+++ examples/json.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::num::ParseIntError;
 use std::str::FromStr;
 
 use nbnf::nbnf;
@@ -7,7 +8,7 @@ use nbnf::nom::bytes::complete::take_while;
 use nbnf::nom::combinator::value;
 
 fn main() {
-	let input = r#"[1, 2.3, "four", {"five": false, "six": 7}, null, "abc \"def\" ghi\n"]"#;
+	let input = r#"[1, -2.3, "four", {"five": false, "six": 0x7}, null, "abc \"def\" ghi\n"]"#;
 	_ = dbg!(json.parse(input));
 }
 
@@ -45,17 +46,17 @@ nbnf!(r#"
 		"false"@<Json::Bool(false)>;
 
 	number<Json> =
-		(number_float / number_int / number_hex)
+		(number_hex / number_float / number_int)
 		|<Json::Number>;
 	number_float<Number> =
-		~([0-9]+ '.' [0-9]*)
+		~('-'? [0-9]+ '.' [0-9]*)
 		|!<|str| f64::from_str(str).map(Number::Float)>;
 	number_int<Number> =
-		~([0-9]+)
+		~('-'? [0-9]+)
 		|!<|str| i128::from_str(str).map(Number::Int)>;
 	number_hex<Number> =
-		(-('0' [xX]) ~([0-9a-fA-F]+))
-		|!<|str| i128::from_str_radix(str, 16).map(Number::Int)>;
+		~('-'? '0' [xX] [0-9a-fA-F]+)
+		|!<parse_number_hex>;
 
 	string<String> =
 		(-'"' string_inner* -'"')
@@ -85,4 +86,18 @@ nbnf!(r#"
 
 fn ws(input: &str) -> IResult<&str, ()> {
 	value((), take_while(char::is_whitespace)).parse(input)
+}
+
+fn parse_number_hex(str: &str) -> Result<Number, ParseIntError> {
+	let negative = str.starts_with("-");
+	let Some(digits_start) = str.find(|c| c == 'x' || c == 'X') else {
+		// it must exist if the parser matched
+		unreachable!()
+	};
+	let digits = &str[digits_start + 1 ..];
+	let mut v = i128::from_str_radix(digits, 16)?;
+	if negative {
+		v *= -1;
+	}
+	Ok(Number::Int(v))
 }
